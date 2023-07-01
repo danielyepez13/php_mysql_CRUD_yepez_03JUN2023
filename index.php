717e5911eb4a2497a44fb406a1bc8947e2de649b
danielyepez13 <danielyepez62@gmail.com>
2023-07-01 12:22:51 -0400

    finish create database and conection with db

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-    <?php
-        include_once("include/header.php");
-    ?>
-        
-     <?php
-        include_once("include/footer.php");
-    ?>
+<?php
+include_once("database/db.php");
+?>
+
+<?php
+include_once("include/header.php");
+?>
+
+<?php
+include_once("include/footer.php");
+?>
